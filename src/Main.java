cee91c8c2901aa57daefeb170cdd207fb06eb112
Chris Skeen <chrisskeen2006@gmail.com>
2025-02-10 08:47:41 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("-----------------------------");
+        System.out.println("Welcome to your ATM!");
+        System.out.println("-----------------------------");
+
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+        System.out.println("-----------------------------");
+
+    }
+}
