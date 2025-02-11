eb9a57fa306983e34fb06a4ee62c40af42edad61
Chris Skeen <chrisskeen2006@gmail.com>
2025-02-11 14:41:02 -0600

    added more logic and incorporated markels signup function

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,27 @@ public class Main {
         System.out.println("-----------------------------");
         System.out.println("Welcome to your ATM!");
         System.out.println("-----------------------------");
-
-        System.out.println("1. Login");
-        System.out.println("2. Register");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
         System.out.println("3. Exit");
         System.out.println("-----------------------------");
 
         String starterOption = scanner.nextLine();
-
-        if (starterOption.equals("1")) {
-            UserLogic userLogic = new UserLogic();
-            userLogic.LogIn();
+        boolean loop = true;
+        while (loop) {
+            if (starterOption.equals("1")) {
+        //          Sign Up
+                UserLogic userLogic = new UserLogic();
+                userLogic.signup();
+            } else if (starterOption.equals("2")) {
+        //          Log In
+                UserLogic userLogic = new UserLogic();
+                userLogic.LogIn();
+            } else if (starterOption.equals("3")) {
+                System.exit(0);
+            } else {
+                System.out.println("Invalid option");
+            }
         }
-
-
-
     }
 }
--- src/UserLogic.java
+++ src/UserLogic.java
@@ -4,12 +4,7 @@ import java.util.Scanner;
 public class UserLogic {
 
     static ConnectToDB connectToDB = new ConnectToDB();
-    public static void signup() {
-
-    }
-
-
-
+//    LOG IN AREA ----------------------------------------------------------
     public static String LogIn() throws SQLException {
         // Create connection, statement and scanner.
         Scanner scanner = new Scanner(System.in);
@@ -40,4 +35,57 @@ public class UserLogic {
         return username;
 
     }
+
+    //    LOG IN AREA ----------------------------------------------------------
+
+    //    SIGN UP AREA ----------------------------------------------------------
+
+    public static String signup() throws SQLException {
+        // Create connection and statement.
+
+        Scanner scanner = new Scanner(System.in);
+        Connection tryConnect = connectToDB.connect();
+        CreateStatement createStatement = new CreateStatement();
+        Statement st = createStatement.ReturnStatement(tryConnect);
+
+
+        System.out.print("Enter new User Name: ");
+        String userName = scanner.nextLine();
+        System.out.print("Enter new User Password: ");
+        String userPassword = scanner.nextLine();
+
+        if (userExists(tryConnect, userName)) {
+            System.out.println("User '" + userName + "' already exists. Please choose a different name.");
+        } else {
+            String query = "INSERT INTO Users (UserName, UserPassword) VALUES (?,?)";
+
+            try {
+                PreparedStatement stmt = tryConnect.prepareStatement(query);
+                stmt.setString(1, userName);
+                stmt.setString(2, userPassword);
+                stmt.executeUpdate();
+                System.out.println("User '" + userName + "' added successfully!");
+                return userName;
+            } catch (SQLException e) {
+            System.out.println("Account was not created!");}
+        }
+        return null;
+    }
+    public static boolean userExists(Connection tryConnect, String userName) throws SQLException {
+        String query = "SELECT 1 FROM users WHERE UserName = ?";
+
+        boolean var5;
+        try (PreparedStatement stmt = tryConnect.prepareStatement(query)) {
+            stmt.setString(1, userName);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                var5 = rs.next();
+            }
+        }
+        return var5;
 }
+//            SIGN UP AREA ----------------------------------------------------------
+
+
+}
+
