d9082c375c2702ab2a2376e8ef3f44368954f572
Chris Skeen <chrisskeen2006@gmail.com>
2025-02-13 11:23:57 -0600

    Added most of the functionality for withdraw, deposit, statements, update atm names, and exiting

--- src/UserLogic.java
+++ src/UserLogic.java
@@ -1,10 +1,15 @@
+import java.math.BigDecimal;
 import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Scanner;
+import java.time.LocalDateTime;
 
 public class UserLogic {
 
     static ConnectToDB connectToDB = new ConnectToDB();
-//    LOG IN AREA ----------------------------------------------------------
+
+    //    LOG IN AREA ----------------------------------------------------------
     public static String LogIn() throws SQLException {
         // Create connection, statement and scanner.
         Scanner scanner = new Scanner(System.in);
@@ -74,10 +79,12 @@ public class UserLogic {
                 System.out.println("User '" + userName + "' added successfully!");
                 return userName;
             } catch (SQLException e) {
-            System.out.println("Account was not created!");}
+                System.out.println("Account was not created!");
+            }
         }
         return null;
     }
+
     public static boolean userExists(Connection tryConnect, String userName) throws SQLException {
         String query = "SELECT 1 FROM users WHERE UserName = ?";
 
@@ -90,10 +97,171 @@ public class UserLogic {
             }
         }
         return var5;
-}
+    }
 //            SIGN UP AREA ----------------------------------------------------------
 
-//
+    //            WITHDRAW AREA ---------------------------------------------------------
+    public static void Withdrawal(String username) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What amount will you be withdrawing?");
+        BigDecimal withdrawAmt = scanner.nextBigDecimal();
+        try {
+//          Get user ID
+            Connection tryConnect = connectToDB.connect();
+            CreateStatement createStatement = new CreateStatement();
+            PreparedStatement preparedStatement = tryConnect.prepareStatement("SELECT * FROM Users WHERE username = ?");
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+//            Withdraw
+
+            if (resultSet.next()) {
+                Integer userID = resultSet.getInt("userID");
+
+                PreparedStatement preparedStatement2 = tryConnect.prepareStatement("SELECT * FROM Atm WHERE UserID = ?");
+                preparedStatement2.setInt(1, userID);
+                ResultSet resultSet2 = preparedStatement2.executeQuery();
+                if (resultSet2.next()) {
+                    BigDecimal atmAmt = resultSet2.getBigDecimal("atmBalance");
+                    if (atmAmt.compareTo(withdrawAmt) >= 0) {
+                        BigDecimal finalNumber = atmAmt.subtract(withdrawAmt);
+
+                        PreparedStatement preparedStatement3 = tryConnect.prepareStatement("UPDATE Atm SET AtmBalance = ? WHERE UserID = ?");
+                        preparedStatement3.setBigDecimal(1, finalNumber);
+                        preparedStatement3.setInt(2, userID);
+                        preparedStatement3.executeUpdate();
+
+                        PreparedStatement preparedStatement4 = tryConnect.prepareStatement("SELECT * FROM Atm WHERE UserID = ?");
+                        preparedStatement4.setInt(1, userID);
+                        ResultSet resultSet4 = preparedStatement4.executeQuery();
+//                        Make new statement
+                        String type = "Withdrawal";
+                        PreparedStatement preparedStatement1 = tryConnect.prepareStatement("INSERT INTO Statements (Type, Amt, UserID, TimeDate) VALUES (?, ?, ?, ?)");
+                        preparedStatement1.setString(1, type);
+                        preparedStatement1.setBigDecimal(2, withdrawAmt);
+                        preparedStatement1.setInt(3, userID);
+                        preparedStatement1.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+                        preparedStatement1.executeUpdate();
+
+
+
+
+
+                        if (resultSet4.next()) {
+                        }
+
+                    } else {
+                        System.out.println("You do not have enough money!");
+                    }
+                }
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void Deposit(String username) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What amount will you be depositing?");
+        BigDecimal depositAmt = scanner.nextBigDecimal();
+        try {
+            Connection tryConnect = connectToDB.connect();
+            CreateStatement createStatement = new CreateStatement();
+            PreparedStatement preparedStatement = tryConnect.prepareStatement("SELECT * FROM Users WHERE username = ?");
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+            }
+            Integer userID = resultSet.getInt("userID");
+            PreparedStatement preparedStatement2 = tryConnect.prepareStatement("SELECT * FROM Atm WHERE UserID = ?");
+            preparedStatement2.setInt(1, userID);
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+            if (resultSet2.next()) {
+            }
+            BigDecimal atmAmt = resultSet2.getBigDecimal("atmBalance");
+            BigDecimal finalNumber = atmAmt.add(depositAmt);
+
+            PreparedStatement preparedStatement3 = tryConnect.prepareStatement("UPDATE Atm SET AtmBalance = ? WHERE UserID = ?");
+            preparedStatement3.setBigDecimal(1, finalNumber);
+            preparedStatement3.setInt(2, userID);
+            preparedStatement3.executeUpdate();
+
+            String type = "Deposit";
+            PreparedStatement preparedStatement1 = tryConnect.prepareStatement("INSERT INTO Statements (Type, Amt, UserID, TimeDate) VALUES (?, ?, ?,?)");
+            preparedStatement1.setString(1, type);
+            preparedStatement1.setBigDecimal(2, depositAmt);
+            preparedStatement1.setInt(3, userID);
+            preparedStatement1.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+            preparedStatement1.executeUpdate();
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void viewStatements(String username) {
+
+        try {
+            Connection tryConnect = connectToDB.connect();
+            CreateStatement createStatement = new CreateStatement();
+
+            PreparedStatement preparedStatement2 = tryConnect.prepareStatement("SELECT * FROM Users WHERE username = ?");
+            preparedStatement2.setString(1, username);
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+            Integer userID = 0;
+            if (resultSet2.next()) {
+                userID = resultSet2.getInt("userID");
+            }
+
+            PreparedStatement preparedStatement = tryConnect.prepareStatement("SELECT * FROM Statements WHERE UserID = ?");
+            preparedStatement.setInt(1, userID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                var timedate = resultSet.getTimestamp("TimeDate");
+                if (timedate != null) {
+                    LocalDateTime localtimedate = timedate.toLocalDateTime();
+                    System.out.println(String.format(resultSet.getString("Type") + ": $" + resultSet.getBigDecimal("Amt") + " on " + localtimedate.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))));
+                } else {
+                    System.out.println(resultSet.getString("Type") + ": $" + resultSet.getBigDecimal("Amt"));
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void updateAtmName(String username) {
+        Connection tryConnect = connectToDB.connect();
+        CreateStatement createStatement = new CreateStatement();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("New ATM name: ");
+        String newAtmName = scanner.nextLine();
+
+        try {
+            PreparedStatement preparedStatement2 = tryConnect.prepareStatement("SELECT * FROM Users WHERE username = ?");
+            preparedStatement2.setString(1, username);
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+            Integer userID = 0;
+            if (resultSet2.next()) {
+                userID = resultSet2.getInt("UserID");
+            }
+
+            PreparedStatement preparedStatement3 = tryConnect.prepareStatement("UPDATE Atm SET AtmName = ? WHERE UserID = ?");
+            preparedStatement3.setString(1, newAtmName);
+            preparedStatement3.setInt(2, userID);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 }
 
